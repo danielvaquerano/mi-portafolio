25c3ac612ec255a7c664e45fa1fe94d6ba444b5d
Daniel <danielvaquerano@gmail.com>
2025-12-11 20:43:18 -0600

    feat: Add vite base config and gh-pages scripts

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Reemplaza 'nombre-del-repo' con el nombre EXACTO de tu repositorio en GitHub
+const repoName = 'nombre-del-repo'; 
+
 export default defineConfig({
   plugins: [react()],
+
+  base: '/mi-portafolio/', 
 })
